bcbc3a4d21ac9a60ed1fe518f0971595a0e1bf6c
fundies <cheeseboy16@gmail.com>
2016-09-22 23:04:44 +0000

    wat

--- travis.sh
+++ travis.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 ./emake --compiler ${COMPILER} --platform ${PLATFORM} --graphics ${GRAPHICS} --audio ${AUDIO} --collision ${COLLISION} --network ${NETWORK} --widgets ${WIDGETS} -o /tmp/test
